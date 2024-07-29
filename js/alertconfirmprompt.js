7356bf701de0739eba193218bbd1148b75dab36e
romulofull <romulo.castro@gmail.com>
2024-07-29 14:36:20 -0500

    Ejercicio Terminado

---
+++ js/alertconfirmprompt.js
@@ -0,0 +1,12 @@
+var respuesta = window.prompt("Programando en Javascript?", "Si, Estoy programando. Me gusta mucho Programar");
+alert ("Su respuesta fue :"  + respuesta);
+
+var confirmacion = window.confirm ("Programando en Javascript?")
+if (confirmacion == true) {
+alert ("SI, FELICIDADES, SIGUE ADELANTE");
+}
+else {
+
+    alert("😭😭😭😭😭");
+}
+
